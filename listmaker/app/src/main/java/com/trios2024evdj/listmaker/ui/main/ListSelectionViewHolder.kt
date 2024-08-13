d089c49b1999ac8b0022377fae98a3d9cf27cb26
Douglas Jasper <doug.jasper@trios.com>
2024-08-12 20:23:58 -0400

    Initial List Displays

---
+++ listmaker/app/src/main/java/com/trios2024evdj/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,9 @@
+package com.trios2024evdj.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.trios2024evdj.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding:
+                              ListSelectionViewHolderBinding
+) : RecyclerView.ViewHolder(binding.root) {
+}
